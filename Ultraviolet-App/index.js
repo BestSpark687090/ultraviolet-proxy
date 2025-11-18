9b0994591dbd326e0c908df383c0daa458b903e0
Ryan Smith <73793381+BestSpark687090@users.noreply.github.com>
2025-11-18 19:13:33 +0000

    technically:

--- Ultraviolet-App/index.js
+++ Ultraviolet-App/index.js
@@ -1,6 +1,6 @@
-import { createServer } from "node:http";
-import { join } from "node:path";
-import { hostname } from "node:os";
+// import { createServer } from "node:http";
+// import { join } from "node:path";
+// import { hostname } from "node:os";
 import wisp from "wisp-server-node";
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
